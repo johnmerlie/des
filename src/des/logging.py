073203c60a9dba87eb6b38f8da31ad66930c4c58
John Merlie <johnmerlie@gmail.com>
2023-11-07 12:03:16 -0500

    update logging package reference

--- src/des/logging.py
+++ src/des/logging.py
@@ -1,6 +1,7 @@
 import logging
+
 from rich.logging import RichHandler
 
-log = logging.getLogger(__package__)
+log = logging.getLogger(__spec__.parent)
 log.setLevel(logging.DEBUG)
 log.addHandler(RichHandler())
