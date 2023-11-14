9bede31c5674ad8f284c1130a1e630447b5757b0
John Merlie <johnmerlie@gmail.com>
2023-11-14 09:05:45 -0500

    restructure

--- tests/test_atomic.py
+++ tests/test_atomic.py
@@ -1,13 +1,11 @@
-from typing import ClassVar
-
 from pydes.atomic import Atomic, StateConstant, StateVariable
-from pydes.model import InputChannel
+from pydes.core import SingleChannelInput
 
 
 def test_initialization():
     class TestModel(Atomic):
         number: int = StateVariable(4)
         const: int = StateConstant(5)
-        input: ClassVar = InputChannel()
+        input = SingleChannelInput()
 
     model = TestModel()
