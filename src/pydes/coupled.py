c12a46af2e948cfa78ed56513d0f0e288a2a3dc0
John Merlie <johnmerlie@gmail.com>
2023-11-13 22:53:11 -0500

    base for coupled devs

--- src/pydes/coupled.py
+++ src/pydes/coupled.py
@@ -1,12 +1,7 @@
-from abc import ABC
 from collections.abc import Set
-from typing import Any
 
-from .model import Model
+from pydes.model import Model
 
 
-class Coupled(ABC):
-    components: Set[Model[Any, Any, Any]]
-
-    def __init__(self, components: Set[Model[Any, Any, Any]]):
-        self.components = components
+class Coupled(Model):
+    components: Set[Model]
