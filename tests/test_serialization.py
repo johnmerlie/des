a45288e8a309ddd5d87d2020d981f442b93017c3
John Merlie <johnmerlie@gmail.com>
2023-11-13 22:56:06 -0500

    serialization tests

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,7 +1,7 @@
+import pickle
 import uuid
 
 from pydes.message import Message
-from pydes.utils import deserialize, serialize
 
 
 def test_initialization():
@@ -9,18 +9,13 @@ def test_initialization():
     timestamp = 1699505064221500572
     data = "some-message"
 
-    msg = Message(destination, timestamp, data)
+    msg = Message(destination=destination, timestamp=timestamp, content=data)
     msg.destination
     assert msg.destination == destination
     assert msg.timestamp == timestamp
 
-    serialized = serialize(msg)
+    serialized = pickle.dumps(msg)
 
-    assert (
-        serialized
-        == b"\xc7Y\x02\x84\xa2id\xd8\x01<\xf1\xa6\xe6\x9c.\x18\x07\xcd\xce\xd3\x95\xad~\x1d\xe8\xabdestination\xd8\x01L>\x1aH>\xaf@\x12\xa4\x80\xbe\x1dT (\xf6\xa9timestamp\xcf\x17\x95\xda\xd9\xfe\xe6d\x9c\xa4data\xacsome-message"
-    )
-
-    deserialized = deserialize(serialized)
+    deserialized = pickle.loads(serialized)
 
     assert deserialized == msg
