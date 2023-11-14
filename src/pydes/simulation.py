fbfb3b7c942eeca65faa7b56539a9a62f6e80a11
John Merlie <johnmerlie@gmail.com>
2023-11-13 22:55:11 -0500

    root class for sim run

--- src/pydes/simulation.py
+++ src/pydes/simulation.py
@@ -2,12 +2,16 @@ from uuid import UUID
 
 import networkx as nx
 
+from .core import Field, Immutable, model_id
 
-class Simulation:
+
+class Simulation(Immutable):
     """The global instantiator for a simulation"""
 
-    id: UUID
-    graph: nx.DiGraph
+    id: UUID = Field(default_factory=model_id)
+    graph: nx.DiGraph = Field(default_factory=nx.DiGraph)
+    num_processes: int = Field(default=1)
+    num_threads: int = Field(default=1)
 
-    def __init__(self):
-        self.graph = nx.DiGraph()
+    def build_model_graph(self):
+        ...
