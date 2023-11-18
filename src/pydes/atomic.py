cd93879d2ada3fae6225f2a85f57f5fa53c562d2
John Merlie <john.merlie@delta.com>
2023-11-17 23:56:54 -0500

    type mods

--- src/pydes/atomic.py
+++ src/pydes/atomic.py
@@ -3,11 +3,17 @@ from typing import Any
 
 from pydantic import Field
 
-from pydes.core import INFINITY, Input, Output, Time
+from pydes.channel import Inputs, Outputs
+from pydes.core import INFINITY, Time
 from pydes.model import Model
 
-StateVariable = partial(Field, frozen=False)
+__all__ = (
+    "StateVariable",
+    "StateConstant",
+    "Atomic",
+)
 
+StateVariable = partial(Field, frozen=False)
 StateConstant = partial(Field, frozen=True)
 
 
@@ -20,14 +26,14 @@ class Atomic(Model):
         """
         return INFINITY
 
-    def external_transition(self, inputs: Input[Any]) -> None:
+    def external_transition(self, inputs: Inputs[Any]) -> None:
         """Override this function to implement custom external transition
 
         Default Implementation returns `state` unchanged.
         """
         pass
 
-    def confluent_transition(self, inputs: Input[Any]):
+    def confluent_transition(self, inputs: Inputs[Any]):
         """Override this function to implement custom confluent transition
 
         Default Implementation processes `internal_transition` followed by
@@ -50,7 +56,7 @@ class Atomic(Model):
         """
         return True
 
-    def output(self) -> Output[Any]:
+    def output(self) -> Outputs[Any]:
         """Override this function to implement custom output function
 
         Default Implementation returns empty map representing no output
